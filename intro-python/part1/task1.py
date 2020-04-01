c3caf6767480ac0f63c07f445bcdb516b9980231
unknown <abrahammachuki@gmail.com>
2020-04-01 15:20:21 +0300

    task1.py

---
+++ intro-python/part1/task1.py
@@ -0,0 +1,7 @@
+def multiply(num1, num2):
+	result=num1 * num2
+	return result
+	
+print("12 * 96 =", multiply(12, 96))
+print("48 * 17 =", multiply(48, 17))
+print("196523 * 87323 =", multiply(196523, 87323))
